168b136a2453cfe83036327fac49443eb28258ae
Marcio Pais <junior.pais@gmail.com>
2026-05-11 23:06:52 -0300

    test: add functional tests for complete sale business flows

---
+++ tests/Ambev.DeveloperEvaluation.Functional/Common/FunctionalWebApplicationFactory.cs
@@ -0,0 +1,36 @@
+using Ambev.DeveloperEvaluation.ORM;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ambev.DeveloperEvaluation.Functional.Common;
+
+public class FunctionalWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.SingleOrDefault(d =>
+                d.ServiceType == typeof(DbContextOptions<DefaultContext>));
+
+            if (descriptor != null)
+                services.Remove(descriptor);
+
+            services.AddDbContext<DefaultContext>(options =>
+                options.UseNpgsql(
+                    "Host=localhost;Port=5432;Database=developer_evaluation_test;Username=developer;Password=ev@luAt10n",
+                    b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.ORM")
+                )
+            );
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<DefaultContext>();
+            db.Database.EnsureCreated();
+        });
+    }
+}
